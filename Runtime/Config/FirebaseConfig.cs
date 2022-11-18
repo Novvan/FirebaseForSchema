e437b3bb2fb6871bef57f7ca92dad65c0fdc192f
Ian Geier <ianallengeier@gmail.com>
2022-11-18 12:59:01 -0300

    remove firebase config dataa

--- Runtime/Config/FirebaseConfig.cs
+++ Runtime/Config/FirebaseConfig.cs
@@ -5,13 +5,13 @@ namespace FirebaseForSchema.Config
 	[CreateAssetMenu(fileName = "FirebaseConfig", menuName = "FirebaseForSchema/Config", order = 0)]
 	public class FirebaseConfig : ScriptableObject
 	{
-		public string apiKey = "AIzaSyBy_1sM0l0FLaUD9pgrXyttkn_F-j9mv1A";
+		public string apiKey;
 
-		public string prodUrl = "https://juanola-5f521.firebaseio.com/";
-		public string devUrl = "https://juanola-5f522.firebaseio.com/";
+		public string prodUrl;
+		public string devUrl;
 		public bool isDebug = true;
 
-		public string authUrl = "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword";
-		public string examId = "offShore";
+		public string authUrl;
+		public string examId;
 	}
 }
